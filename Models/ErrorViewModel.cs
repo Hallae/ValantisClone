34e11ebd2fe1e8206c0b5bd09f4c81583844bdfb
Oxhoba Mcdowall <mr.tabby@hotmail.com>
2024-02-24 22:16:28 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ValantisClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
